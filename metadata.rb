97b08eead99e1881e4f4c43a07f62b1dd6257fc4
Darron Froese <darron@froese.org>
2014-01-02 13:06:26 -0700

    First commit.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+name             'ruby2-app-server'
+maintainer       'Darron Froese'
+maintainer_email 'darron@froese.org'
+license          'Apache 2.0'
+description      'Installs/configures something'
+version          '0.1.0'
+recipe           'ruby2-app-server::default', 'Installs/configures something'
